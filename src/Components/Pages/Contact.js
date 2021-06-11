35133b20c7de2946d71149071784de65de85e8ef
OzB7505 <obal7505@richmondhillhs.org>
2021-06-11 03:21:00 +0000

    teacher contact added

--- src/Components/Pages/Contact.js
+++ src/Components/Pages/Contact.js
@@ -1,17 +1,62 @@
-  import React from 'react';
+import React from 'react';
+import Teacherinfo from './Teacherinfo';
+import  '../../css/Card.css';
 
-const Contact = () => {
+
+const Contact = ({title,imageUrl,body}) => {
   return (
+    <div>
     <div
       style={{
         display: 'flex',
         justifyContent: 'center',
         alignItems: 'center',
-        height: '90vh'
       }}
     >
+       <div>
       <h1>Contact Us</h1>
-    </div>
+      </div> </div>
+      <div className="container" >
+          <h3 class="centertext">12th grade</h3>
+        <ul className="cards"class ="gradedivide"
+      ><Teacherinfo
+          imageUrl='Image for each teacher'
+          title ='Dominguez'
+          body='Teacher for Computer Science and the City'
+          email ='Email: jdominguez@richmondhillhs.org '
+          />
+      </ul>
+      <h3 class="centertext">11th grade</h3>
+      <ul className="cards"class ="gradedivide">
+        <Teacherinfo
+          imageUrl='../../images/Canzoneri.jpg'
+          title ='Canzoneri'
+          body='Teacher for AP computer science principles'
+          email ='Email: rcanzoneri@richmondhillhs.org'
+          />
+      </ul>
+          <h3 class="centertext">10th grade</h3>
+      <ul className="cards"class ="gradedivide">
+        <Teacherinfo
+          imageUrl='Image for each teacher'
+          title ='Joseph'
+          body='Teacher for sophmore year'
+          email ='?'
+          />
+
+      </ul>
+      <h3 class="centertext">9th grade</h3>
+      <ul className="cards"class ="gradedivide">
+        <Teacherinfo
+          imageUrl='Image for each teacher'
+          title ='?'
+          body='Teacher for freshmen year'
+          email ='?'
+          />
+      </ul>
+      </div>
+      </div>
+    
   );
 };
 
---
+++ src/Components/Pages/Teacherinfo.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import  '../../css/Card.css';
+
+function Teacherinfo({title,imageUrl,body,email,}) {
+    return(       
+<div className= 'card-container'>
+        <div className= "image-container">
+        <img src= {imageUrl} alt='' />
+        </div>
+    <div className= "card-content">
+        <div className="card-title">
+         <h3>{title}</h3>
+        </div>
+        <div className= "card-body">
+            <p>{body}</p>
+        </div>
+        <div className= "card-body">
+            <p>{email}</p>
+        </div>
+    </div>    
+</div>
+
+    )
+}
+
+export default Teacherinfo; 
